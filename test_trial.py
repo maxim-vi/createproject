0f15796a92fb3b75da4489ee2502e83a2dbf17c3
maxim.titov <maxim.titov@megafon.ru>
2024-11-13 16:24:30 +0300

    change selene!

--- test_trial.py
+++ test_trial.py
@@ -5,5 +5,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+browser.element('[name="q"]').should(be.blank).type('yashaka/selene!').press_enter()
 browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
